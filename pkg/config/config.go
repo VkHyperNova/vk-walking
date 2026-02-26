6dbf16478216699f28c333b76092ac410e64e7bc
Veikko <kiis.veikko@gmail.com>
2026-02-26 13:38:21 +0200

    Update default content and file paths for walkings.json

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,11 +2,11 @@ package config
 
 import "path/filepath"
 
-var DefaultContent = `{"fastings": []}`
+var DefaultContent = `{"walkings": []}`
 
-var	file = "fasting.json"
+var	file = "walkings.json"
 var baseLocal = "DATABASES"
 var	baseBackup = "/media/veikko/VK DATA/"
 
-var LocalFile = filepath.Join(baseLocal, "FASTING", file)
-var BackupFile = filepath.Join(baseBackup, "DATABASES", "FASTING", file)
+var LocalFile = filepath.Join(baseLocal, "WALKINGS", file)
+var BackupFile = filepath.Join(baseBackup, "DATABASES", "WALKINGS", file)
